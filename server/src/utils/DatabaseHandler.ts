b7909f3852dc210d7a36a7b66fed40ec8b9dd162
Szabó Martin <martin77szabo@gmail.com>
2020-10-27 11:38:47 +0100

    user authentication

--- server/src/utils/DatabaseHandler.ts
+++ server/src/utils/DatabaseHandler.ts
@@ -15,21 +15,23 @@ export default class DatabaseHandler {
     this.prisma = new PrismaClient();
   }
 
-  async registerUser(email: string, password: string, name?: string): Promise<User> {
-    // TODO Check if user already exists
-    return await hashPassword(password).then((hash) => {
-      return this.prisma.user.create({
-        data: { email, password: hash, name },
-      });  
-    })
+  async registerUser(email: string, password: string, name?: string): Promise<User | DatabaseError> {
+    return await this.authenticateUser(email, password).then((succes) => {
+      if (typeof succes === typeof DatabaseError) {
+        return hashPassword(password).then((hash) => {
+          return this.prisma.user.create({
+            data: { email, password: hash, name },
+          });  
+        })
+      } else {
+        return new DatabaseError('User already exists')
+      }
+    })   
   }
 
-  async createNewPair(
-      email: string,
-      password: string
-    ): Promise<Pair | DatabaseError> {
-    
+  async createNewPair(email: string, password: string): Promise<Pair | DatabaseError> {
     // Find the user in the database (also authentication)
+    // TODO getUser()
     const user = await this.prisma.user.findFirst({
       where: { email, password },
       select: {
@@ -41,7 +43,7 @@ export default class DatabaseHandler {
 
     if (user) {
       if (user.createdPair || user.joinnedToPair) {
-        return new DatabaseError("The user already has a pair.");
+        return new DatabaseError("The user already has a pair");
       } else { // User does not belongs to a pair
         // Creating a new pair
         return await this.prisma.pair.create({
@@ -55,9 +57,24 @@ export default class DatabaseHandler {
       }
 
     } else { // User not found with the given email & pw
-      throw new DatabaseError(
-        "User authentication failed, or the user does not exists."
-      );
+      throw new DatabaseError("User authentication failed, or the user does not exists");
     }
+  }
+
+  async authenticateUser(email: string, password: string): Promise<Boolean | DatabaseError> {
+    return await this.prisma.user.findOne({
+      where:  { email },
+      select: { password: true }
+    }).then((user) => {
+      
+      if (!user) return new DatabaseError("User does not exits");
+      // ! DO NOT tell the client the email is not exists.
+      // ! Just send back "Email or password is incorrect"
+      // ! They don't have to know everything. It's more secure this way.
+
+      return comparePassword(password, user.password).then((success) => {
+        return success
+      })   
+    })
   }
 }
