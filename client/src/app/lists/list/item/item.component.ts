bfbda70878e86b5cd8a393b4528eecfb7b4fe988
Kelileis <devleczo@gmail.com>
2021-01-05 06:42:13 +0100

    Item connected

--- client/src/app/lists/list/item/item.component.ts
+++ client/src/app/lists/list/item/item.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { SocketService } from '../../../core/services/socket-service.service';
+import { AuthService } from '../../../core/services/auth.service'
 
 @Component({
   selector: 'app-item',
@@ -7,31 +9,61 @@ import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class ItemComponent implements OnInit {
 
+  token: string = ''
+
   @Input() data: any;
   @Output() change = new EventEmitter<string>();
   @Output() delete = new EventEmitter<string>();
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private socketService: SocketService
+  ) {
+
+  }
 
   ngOnInit(): void {
     this.data.quantity = this.data.quantity || 1
+
+    this.authService.getUserToken().subscribe((data: any) => {
+      this.token = data
+      console.debug(data)
+    })
   }
 
-  check(): void {
+  check(itemId: number): void {
     this.data.checked = !this.data.checked
     this.change.emit(this.data.name)
+
+    this.socketService.send("doneItem", {
+      token: this.token,
+      itemId,
+      done: this.data.checked
+    })
   }
 
-  _delete():void {
+  _delete(): void {
     this.delete.emit(this.data.name)
   }
 
-  incQuantity():void {
+  incQuantity(itemId: number): void {
     this.data.quantity += 1
+
+    this.socketService.send("changeQuantity", {
+      token: this.token,
+      itemId,
+      quantity: this.data.quantity
+    });
   }
 
-  decQuantity():void {
+  decQuantity(itemId: number): void {
     this.data.quantity = Math.max(1, this.data.quantity - 1)
+
+    this.socketService.send("changeQuantity", {
+      token: this.token,
+      itemId,
+      quantity: this.data.quantity
+    });
   }
 
 }
